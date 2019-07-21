d757188ac927d7185aa0af04c8951581a8d22b12
charmainehb <charmainebatulan@gmail.com>
2019-07-21 18:17:00 +1000

    update and delete functionality WIP

--- public/js/jobListingDetail.js
+++ public/js/jobListingDetail.js
@@ -7,9 +7,11 @@ var skillsRequirement = document.getElementById("skillsRequirement");
 var marksRequirement = document.getElementById("marksRequirement");
 var expiryDate = document.getElementById("expiryDate");
 
-function onLoad() {
-    var queryString = decodeURIComponent(window.location.search);
+var queryString = decodeURIComponent(window.location.search);
     queryString = queryString.substring(1);
+
+function onLoad() {
+    
     console.log("Document ID ", queryString);
 
     var docRef = db.collection("jobListing").doc(queryString);
@@ -17,13 +19,13 @@ function onLoad() {
     docRef.get().then(function(doc) {
         if (doc.exists) {
             console.log("Document data:", doc.data());
-            title.innerHTML = doc.data().role + " for " + doc.data().courseCode;
-            courseName.innerHTML = doc.data().courseName;
-            courseDescription.innerHTML = doc.data().courseDescription;
-            experienceRequirement.innerHTML = doc.data().experienceRequirement;
-            skillsRequirement.innerHTML = doc.data().skillsRequirement;
-            marksRequirement.innerHTML = doc.data().marksRequirement;
-            expiryDate.innerHTML = doc.data().expiryDate;
+            // title.innerHTML = doc.data().role + " for " + doc.data().courseCode;
+            courseName.value = doc.data().courseName;
+            courseDescription.value = doc.data().courseDescription;
+            experienceRequirement.value = doc.data().experienceRequirement;
+            skillsRequirement.value = doc.data().skillsRequirement;
+            marksRequirement.value = doc.data().marksRequirement;
+            expiryDate.value = doc.data().expiryDate;
         } else {
             // doc.data() will be undefined in this case
             console.log("No such document!");
@@ -32,7 +34,30 @@ function onLoad() {
         console.log("Error getting document:", error);
     });
 
+}
 
+function deleteListing(){
+
+    db.collection("jobListing").doc(queryString).delete().then(function() {
+        window.alert("Document successfully deleted!");
+        window.location.assign("joblistings.html");
+    }).catch(function(error) {
+        console.error("Error removing document: ", error);
+    });
+
+}
+
+function updateListing(){
+
+    db.collection("jobListing").doc(queryString).update({
+
+        courseName: courseName.value
+
+    }).catch(function(error) {
+        console.error("Error removing document: ", error);
+    });
+
+    window.alert("yas");
 
 }
 
