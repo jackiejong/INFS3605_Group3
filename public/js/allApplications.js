d8c630ad5a45c6bc5c57a3a5ceb398119bac0d23
charmainehb <charmainebatulan@gmail.com>
2019-07-25 07:47:17 +1000

    allApplications page

---
+++ public/js/allApplications.js
@@ -0,0 +1,74 @@
+var db = firebase.firestore();
+
+function onLoad() {
+
+
+    var arrHead = new Array();
+    arrHead = ['Course Code', 'Role', 'Responsibilities', 'Skill Requirement','Experience Requirement','WAM Requirement',''];      // SIMPLY ADD OR REMOVE VALUES IN THE ARRAY FOR TABLE HEADERS.
+
+    // FIRST CREATE A TABLE STRUCTURE BY ADDING A FEW HEADERS AND
+    // ADD THE TABLE TO YOUR WEB PAGE.
+    
+        var empTable = document.createElement('table');
+        empTable.setAttribute('id', 'empTable');            // SET THE TABLE ID.
+
+        var tr = empTable.insertRow(-1);
+
+        for (var h = 0; h < arrHead.length; h++) {
+            var th = document.createElement('th');          // TABLE HEADER.
+            th.innerHTML = arrHead[h];
+            tr.appendChild(th);
+        }
+
+        
+        
+        db.collection("jobListing").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                console.log(`${doc.id} => ${doc.data()}`);
+                    var tr = empTable.insertRow(-1);
+
+                    var td = document.createElement('td');
+                    td.innerHTML = doc.data().courseCode;
+                    tr.appendChild(td);
+
+                    td = document.createElement('td');
+                    td.innerHTML = doc.data().role;
+                    tr.appendChild(td);
+
+                    td = document.createElement('td');
+                    td.innerHTML = doc.data().responsibilities;
+                    tr.appendChild(td);
+
+                    td = document.createElement('td');
+                    td.innerHTML = doc.data().skillsRequirement;
+                    tr.appendChild(td);
+
+                    td = document.createElement('td');
+                    td.innerHTML = doc.data().experienceRequirement;
+                    tr.appendChild(td);
+
+                    td = document.createElement('td');
+                    td.innerHTML = doc.data().marksRequirement;
+                    tr.appendChild(td);
+
+                    
+                    td = document.createElement('td');
+                    var button = document.createElement('button');
+                    console.log(typeof doc.id);
+                    var theLink = 'onClick("' + 'jobListingDetail.html?' + doc.id + '")';
+                    console.log(typeof theLink);
+                    button.setAttribute("onclick",theLink);
+                    button.setAttribute("class","btn btn-dark");
+                    button.innerHTML = "View Details";
+                    td.appendChild(button);
+                    tr.appendChild(td);
+                    
+
+            });
+        });
+
+        var div = document.getElementById('insertTableHere');
+
+        div.appendChild(empTable);    // ADD THE TABLE TO YOUR WEB PAGE.
+    
+    }
